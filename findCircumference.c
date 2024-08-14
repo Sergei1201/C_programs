37a9ef532bd37ef3e3a6d57ade88825c0fb264f5
Sergei Sokolov <sokolov12ssa@gmail.com>
2024-08-14 21:41:11 +0500

    calculate area

--- findCircumference.c
+++ findCircumference.c
@@ -2,8 +2,16 @@
 #include <math.h>
 #include <stdbool.h>
 
-// Function prototyping
+/* Global variables */
+const double PI = 3.14;
+
+/* Function prototyping */
+
+// Calculate circumference
 double findCircumference(double radius);
+
+// Calculate area
+double findArea(double radius);
 
 // Main function
 int main(void)
@@ -11,17 +19,24 @@ int main(void)
     // Variable declaration
     double radius;
     double circumference;
+    double area;
     printf("Enter the radius of a circle to calculate the circumference: ");
     scanf("%lf", &radius);
     // Invoke the function defined down bellow
     circumference = findCircumference(radius);
+    area = findArea(radius);
     printf("The circumference is: %.2lf\n", circumference);
+    printf("The area is: %2.lf\n", area);
     return 0;
 }
 
-// Function definition
+/* Function definition */
 double findCircumference(double radius)
 {
-    const double PI = 3.14;
     return 2 * PI * radius;
+}
+
+double findArea(double radius)
+{
+    return PI * radius * radius;
 }
